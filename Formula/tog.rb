31c8ae1923dbe857768351c8a3b6aa6028139fda
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-16 10:50:53 -0300

    Brew formula update for tog version v0.3.1

--- Formula/tog.rb
+++ Formula/tog.rb
@@ -2,21 +2,21 @@
 class Tog < Formula
   desc "Tog CLI"
   homepage "https://github.com/escaletech/tog-cli"
-  version "0.3.0"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/escaletech/tog-cli/releases/download/v0.3.0/tog_0.3.0_macOS_amd64.tar.gz"
-    sha256 "ea4198b3299e7ca011955f29245ec50bb31ab654797d1af1ca2ce77f84167919"
+    url "https://github.com/escaletech/tog-cli/releases/download/v0.3.1/tog_0.3.1_macOS_amd64.tar.gz"
+    sha256 "aa88701e1c1eacd3f3c9cbcba6e37df68b08d8808f8b70464c475c87ffbfd3df"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/escaletech/tog-cli/releases/download/v0.3.0/tog_0.3.0_linux_amd64.tar.gz"
-      sha256 "d4daa6aa1abb44983a7e59c5c025e57b2a8fd372517ef352789f5e6ae4a1d0be"
+      url "https://github.com/escaletech/tog-cli/releases/download/v0.3.1/tog_0.3.1_linux_amd64.tar.gz"
+      sha256 "b66db0e0fb22dfb6e6c06286612ac34bc29752770ab4161ecc65e52824525ae6"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/escaletech/tog-cli/releases/download/v0.3.0/tog_0.3.0_linux_arm64.tar.gz"
-        sha256 "e13d3d3d8dcc62ef065cae6a0f42fb804655519098e7384e0aab569b2235c75b"
+        url "https://github.com/escaletech/tog-cli/releases/download/v0.3.1/tog_0.3.1_linux_arm64.tar.gz"
+        sha256 "b45522f47f73d122d2def8103395cd1913235c0b1a3bba431ba40f94d552e75e"
       else
       end
     end
